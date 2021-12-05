b872649827a258a30fdab0e92b6d0b1c8be61fb2
Chirayu Sharma <thechirayu02@gmail.com>
2021-12-05 14:12:47 +0530

    Added static ball image & background color

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,36 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.lightBlueAccent.shade200,
+          appBar: AppBar(
+            title: Text('Ask Me Anything'),
+            backgroundColor: Colors.blue,
+          ),
+          body: MagicBall(),
+        ),
       ),
     );
+
+class MagicBall extends StatefulWidget {
+  const MagicBall({Key? key}) : super(key: key);
+
+  @override
+  _MagicBallState createState() => _MagicBallState();
+}
+
+class _MagicBallState extends State<MagicBall> {
+  int ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Image.asset('images/ball1.png'),
+        ),
+      ),
+    );
+  }
+}
